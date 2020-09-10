71d473e267d422ec19aec47f036d298555ef2fc2
Susanna Hardee <susannahardee@Susannas-MBP.fios-router.home>
2020-09-10 11:14:16 -0400

    firstchallengeproblems

---
+++ src/madeira/cs/Main.java
@@ -0,0 +1,12 @@
+package madeira.cs;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String mystring = "SusannaLynnHardee";
+        System.out.println(mystring);
+
+    }
+}
+
+

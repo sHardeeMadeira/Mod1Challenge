06b0d568d9859beea1872aa3418a148faaad041b
Peter Wallace <pwallace@madeira.org>
2020-09-15 09:37:36 -0400

    returning graded work

--- src/madeira/cs/Main.java
+++ src/madeira/cs/Main.java
@@ -7,10 +7,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Good thought here but the correct answer is to use the .print() method so it PRINTS on one line
         String first = "Susanna"; String middle = "Lynn"; String last = "Hardee";
         System.out.println(first); System.out.println(middle); System.out.println(last);
 
-
+        // Interesting declaration of System here. Not necessary but not bad
         System system = null;
         system.out.print ("hello \n" + "world");
 
@@ -18,6 +19,8 @@ public class Main {
         system1.out.print ("\nI am 15 years old");
 
 
+        // The goal is to get you to guess! You need to compare the name to your name and
+        // print the result of the boolean comparison
         Scanner myScanner = new Scanner(System.in);
         System.out.print("Input your first name: ");
         String Susanna = myScanner.next();
